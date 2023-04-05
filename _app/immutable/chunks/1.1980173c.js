c1ab92644755e858b2e19451307d683430ed3cc3
Daniel S. Park <soohan@heartade.dev>
2023-04-05 09:52:18 +0900

    Updates

--- _app/immutable/chunks/1.1980173c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.0eb71f69.js";export{t as component};
---
+++ _app/immutable/chunks/paths.94e263ce.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_xey7qq)==null?void 0:s.base)??"/vxltr";var e;const a=((e=globalThis.__sveltekit_xey7qq)==null?void 0:e.assets)??t;export{a,t as b};
